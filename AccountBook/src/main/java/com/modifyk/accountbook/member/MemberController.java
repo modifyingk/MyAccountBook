40ecdd47589749af82dbddd5f5649ee40e5e8288
modifyingk <116068100+modifyingk@users.noreply.github.com>
2023-08-07 14:40:28 +0900

    인증번호 이메일 전송 및 확인

--- AccountBook/src/main/java/com/modifyk/accountbook/member/MemberController.java
+++ AccountBook/src/main/java/com/modifyk/accountbook/member/MemberController.java
@@ -3,6 +3,8 @@ package com.modifyk.accountbook.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
@@ -11,6 +13,9 @@ public class MemberController {
 	
 	@Autowired
 	MakeCodeService makeCodeSvc;
+	
+	@Autowired
+	SendMailService sendMailSvc;
 	
 	// 아이디 중복 확인
 	@RequestMapping("member/isOverlapId")
@@ -24,11 +29,24 @@ public class MemberController {
 	}
 	
 	// 이메일 인증번호 전송
+	@ResponseBody
 	@RequestMapping("member/sendCode")
-	public void sendCode(String email) {
+	public String sendCode(String email) {
 		String code = makeCodeSvc.makeCode(); // 6자리 인증번호 생성 service
 		System.out.println(code);
-		// code 전송
 		
+		System.out.println(email);
+		
+		String from = "sujung6812@gmail.com";
+		String subject = "[Accountbook] 이메일 인증번호";
+		String text = "[Accountbook] 이메일 인증을 위한 인증번호입니다.\n 인증번호 : " + code;
+		
+		String result = sendMailSvc.sendMail(email, from, subject, text);
+		
+		if(result.equals("success")) {
+			return code;
+		} else {
+			return "fail";
+		}
 	}
 }
---
+++ AccountBook/src/main/webapp/WEB-INF/views/member/sendCode.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<i style="color: red;">전송 성공</i>
