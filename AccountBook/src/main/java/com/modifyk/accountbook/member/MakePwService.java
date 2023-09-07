ddb9f3c4ba52beee67ce016908e3a1057e4b391c
modifyingk <116068100+modifyingk@users.noreply.github.com>
2023-09-07 15:19:33 +0900

    임시 비밀번호 생성 방법 수정 (6자리 숫자 -> 알파벳/숫자+특수문자 형식)

--- AccountBook/src/main/java/com/modifyk/accountbook/member/MakePwService.java
+++ AccountBook/src/main/java/com/modifyk/accountbook/member/MakePwService.java
@@ -7,8 +7,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class MakePwService {
 	public String makePw() {
+		char[] ch = {'a', 'b', 'c', 'd', 'e', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+					'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+		char[] ch2 = {'~', '!', '@', '#', '$', '%', '^', '&', '*', '?'};
+		
 		Random rand = new Random();
-		String code = String.valueOf(rand.nextInt(900000) + 100000);
+		
+		String code = "";
+		for(int i = 0; i < 9; i++) {
+			code += ch[rand.nextInt(ch.length)];
+		}
+		code += ch2[rand.nextInt(ch2.length)];
+		
+		System.out.println("생성된 코드 : " + code);
 		return code;
 	}
 }
