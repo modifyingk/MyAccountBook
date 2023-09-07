70933e807a90f6d45ba0bbf230ff115e5c95c884
modifyingk <116068100+modifyingk@users.noreply.github.com>
2023-09-07 14:56:02 +0900

    수입/지출 내역 날짜순 정렬

--- AccountBook/src/main/java/com/modifyk/accountbook/account/AccountToMapService.java
+++ AccountBook/src/main/java/com/modifyk/accountbook/account/AccountToMapService.java
@@ -1,6 +1,7 @@
 package com.modifyk.accountbook.account;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ public class AccountToMapService {
 		// accountList를 날짜별로 모아줌 (key값은 날짜, value는 수입/지출, 카테고리, 내용, 돈)
 		// 해당 날짜에 내역이 여러개 있으면 콤마(,)로 내역을 구별
 		// 수입/지출, 카테고리, 내용 돈은 샵(#)으로 구별하여 map에 저장
-		HashMap<String, Object> map = new HashMap<String, Object>();
+		HashMap<String, Object> map = new LinkedHashMap<String, Object>();
 		
 		for(int i = 0; i < accountList.size(); i++) {
 			if(map.get(accountList.get(i).getDate()) != null) { // 해당 날짜가 이미 있다면
