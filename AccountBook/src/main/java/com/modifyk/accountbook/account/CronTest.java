a04da2e0ac1c2d09c02bc86e298dcc9674699703
modifyingk <116068100+modifyingk@users.noreply.github.com>
2023-10-10 00:02:47 +0900

    반복 기능 매년, 매월, 매주, 매일 v1 (보완 필요)

--- AccountBook/src/main/java/com/modifyk/accountbook/account/CronTest.java
+++ AccountBook/src/main/java/com/modifyk/accountbook/account/CronTest.java
@@ -19,7 +19,9 @@ public class CronTest {
 		// 현재 날짜 구하기
         LocalDate now = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("E");
         String today = now.format(formatter);
+        String todayDay = now.format(formatter2);
         
 		List<RepeatVO> list = rDao.repeatAll();
 		
@@ -35,7 +37,41 @@ public class CronTest {
 				accountVO.setUserid(list.get(i).getUserid());
 				aDao.insertAccount(accountVO);
 				
-				System.out.println(accountVO);
+			} else if(list.get(i).getRepeatcycle().contains("매월")) {
+				// 현재 일자와 반복 일자가 같으면
+				if(today.split("-")[2].equals(list.get(i).getRepeatcycle().split(" ")[1])) {
+					accountVO.setAstname(list.get(i).getAstname());
+					accountVO.setCatename(list.get(i).getCatename());
+					accountVO.setContent(list.get(i).getContent());
+					accountVO.setDate(today);
+					accountVO.setMoneytype(list.get(i).getMoneytype());
+					accountVO.setTotal(list.get(i).getTotal());
+					accountVO.setUserid(list.get(i).getUserid());
+					aDao.insertAccount(accountVO);
+				}
+			} else if(list.get(i).getRepeatcycle().contains("매년")) {
+				// 현재 연월과 반복 연월이 같으면
+				if((today.split("-")[1] + "/" + today.split("-")[2]).equals(list.get(i).getRepeatcycle().split(" ")[1])) {
+					accountVO.setAstname(list.get(i).getAstname());
+					accountVO.setCatename(list.get(i).getCatename());
+					accountVO.setContent(list.get(i).getContent());
+					accountVO.setDate(today);
+					accountVO.setMoneytype(list.get(i).getMoneytype());
+					accountVO.setTotal(list.get(i).getTotal());
+					accountVO.setUserid(list.get(i).getUserid());
+					aDao.insertAccount(accountVO);
+				}
+			} else if(list.get(i).getRepeatcycle().contains("매주")) {
+				if(todayDay.equals(list.get(i).getRepeatcycle().split(" ")[1])) {
+					accountVO.setAstname(list.get(i).getAstname());
+					accountVO.setCatename(list.get(i).getCatename());
+					accountVO.setContent(list.get(i).getContent());
+					accountVO.setDate(today);
+					accountVO.setMoneytype(list.get(i).getMoneytype());
+					accountVO.setTotal(list.get(i).getTotal());
+					accountVO.setUserid(list.get(i).getUserid());
+					aDao.insertAccount(accountVO);
+				}
 			}
 		}
 	}
